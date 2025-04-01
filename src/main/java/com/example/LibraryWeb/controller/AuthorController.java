558721915e9e7ca279711a29f479c780c39ff115
305-11 <305-11@gmail.com>
2025-04-01 11:38:35 +0300

    Добавлен список авторов

--- src/main/java/com/example/LibraryWeb/controller/AuthorController.java
+++ src/main/java/com/example/LibraryWeb/controller/AuthorController.java
@@ -1,10 +1,13 @@
 package com.example.LibraryWeb.controller;
 
 import com.example.LibraryWeb.service.AuthorService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorController {
@@ -19,5 +22,18 @@ public class AuthorController {
     String author(@PathVariable("id") Long id, Model model){
         model.addAttribute("author", authorService.findById(id));
         return "author";
+    }
+
+    @GetMapping("/author")
+    String authorList(Model model,
+                    @RequestParam(defaultValue = "") String search,
+                    @RequestParam(defaultValue = "0") @Min(0) Integer offset,
+                    @RequestParam(defaultValue = "20") @Min(1) @Max(100) Integer limit){
+        model.addAttribute("search", search);
+        model.addAttribute("authors", !search.isBlank() ?
+                authorService.findByName(search, offset, limit) : authorService.findAll(offset, limit));
+        model.addAttribute("pageTitle", !search.isBlank() ?
+                String.format("Поиск - %s", search) : "Список авторов");
+        return "authorList";
     }
 }
